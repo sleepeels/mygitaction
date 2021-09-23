b3ba7df925efd0f94d1759c15474361092f60953
Lior <lior.webdev@gmail.com>
2021-09-23 13:35:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const printme = () => {
   console.log("this is my name");
 }
 const printme2 = () => {
-   console.log("this is my 2 name"); 
+  console.log("this is my 2 name"); 
 }
 printme();
 printme2();
